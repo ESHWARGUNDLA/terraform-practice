614647796cb4785e3808c0520268339ebe75540c
eshwargundla@gmail.com <eshwargundla@gmail.com>
2025-07-18 17:48:43 +0000

    new

--- resource.tf
+++ resource.tf
@@ -1,7 +1,7 @@
 # Create Resource Group
 
-resource "azurerm_resource_group" "rg" {
-  name     = "example-resources1"
+resource "azurerm_resource_group" "main" {
+  name     = "example-resources"
   location = "East US"
 }
 
@@ -10,15 +10,15 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_virtual_network" "rg" {
   name                = "vnet-terraform-vm"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
 
 }
 
 # Create Subnet
 resource "azurerm_subnet" "internal" {
   name                 = "subnet-internal"
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.rg.name
   address_prefixes     = ["10.0.2.0/24"]
 }
